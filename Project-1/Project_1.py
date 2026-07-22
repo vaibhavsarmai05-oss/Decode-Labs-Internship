b8a1b68943393896bda1c34c02dc629be6c1293e
vaibhavsarmai <vaibhavsarmai05@gmail.com>
2026-07-23 00:36:19 +0530

    Upload Rule-Based AI Chatbot project

---
+++ Project-1/Project_1.py
@@ -0,0 +1,26 @@
+print("="*40)
+print("   Welcome to AI Chatbot")
+print("="*40)
+print("Type 'exit' or 'end' to stop\n")
+response = {
+    "hi": "Bot: Hello! What's up",
+    "hello": "Bot: Hi! How are you",
+    "all good" : "Bot: Nice",
+    "i am fine" : "Bot: Good to hear that",
+    "who are you": "Bot: I am a Rule-Based Chatbot",
+    "help": "Bot: Try asking about AI , Python or Hi",
+    "ai" : "Bot: Artificial Intelligence",
+    "python" : "Bot: It is a programming language"
+}
+user = input("Bot: Ask me something: ")
+while True:
+    cleaned_user = user.lower().strip()
+    if(cleaned_user == "exit" or cleaned_user == "end"):
+        print("Bot: Thank you for chatting! Goodbye")
+        print("="*40)
+        break
+    elif(cleaned_user in response):
+        print(response[cleaned_user])
+    else:
+        print("Bot: Sorry! I don't understand it")
+    user = input("You: ")
---
+++ Project-1/README.md
@@ -0,0 +1,17 @@
+# Rule-Based AI Chatbot
+
+## Description
+A simple Rule-Based AI Chatbot built using Python.
+
+## Features
+- Greets the user
+- Uses a dictionary for responses
+- Handles unknown inputs
+- Exit command
+- Uses loops and if-else statements
+
+## Technologies
+- Python
+
+## Author
+Vaibhav Sarmai
